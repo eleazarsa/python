6be2af4720af2a3b7ee02a2747aa0889e560fbd2
Eleazar <43405197+eleazarsa@users.noreply.github.com>
2018-11-12 16:27:39 +0000

    1.0

--- faltas/funciones.py
+++ faltas/funciones.py
@@ -1,6 +1,6 @@
 #                                                        #
 #              Eleazar Sánchez Arbelo                    #
-#													     #
+#								             12/11/18	 #
 ##########################################################
 
 import crayons
@@ -29,7 +29,7 @@ def Falta(profesores):	#Funcion que sustituye a los profesores.
 	for ii in range (0,len(profesores)):
 		if profesores[ii].d and profesores[ii].hour[dia][hora] == 'LIBRE':
 			print (crayons.blue(profesores[ii].nombre) ,crayons.blue( 'puede sustitur'))
-	
+
 def Mostrar(profesores):	#Muestra los horarios
 	print('')
 	print (crayons.white('Profesores guardados: '))
@@ -46,8 +46,8 @@ def Mostrar(profesores):	#Muestra los horarios
 	for j in range(0,5):
 		print(crayons.blue(profesores[aux2].hour[0][j]))
 		print(crayons.green(profesores[aux2].hour[j+1]))
-	
-def Ver(i,j):	#Ver dias en la recogida de datos 
+
+def Ver(i,j):	#Ver dias en la recogida de datos
 	if i==1:
 		print (' LUNES     ' ,j+1, 'h')
 	if i==2:
@@ -63,9 +63,9 @@ def Crear_profesor(f,Profesor):	#Guarda el profesor en fichero
 	f.write('$')
 	prof=Profesor()
 	prof.nombre=input('Nombre : ')
-	f.write(prof.nombre) 
+	f.write(prof.nombre)
 	Crear_horario(f,prof)
-	
+
 def Crear_horario (f,profesora):	#Guarda horario en fichero
 	print('')
 	print(crayons.green('Introduzca el horario : '))
@@ -73,10 +73,10 @@ def Crear_horario (f,profesora):	#Guarda horario en fichero
 		for j in range (0,6):
 			Ver(i,j)
 			profesora.hour[i][j]= input()
-			f.write(','+profesora.hour[i][j]) 
+			f.write(','+profesora.hour[i][j])
 	f.write('\n')
 
-def Conver(aux,a):	#de array a profesor.hour	
+def Conver(aux,a):	#de array a profesor.hour
 	n=1
 	for i in range (1,6):
 		for j in range(0,6):
@@ -91,6 +91,3 @@ def Manual():
 	print(crayons.green('Si quiere eliminar un profesor, eliminelo del archivo faltas.txt y no deje ninguna linea entre el resto de los profesores guardados.'))
 	print(crayons.green('Si añade profesor/es nuevo/s, antes de realizar operaciones con este/os nuevo/s profesor/es debe reiniciar el programa.'))
 	print('')
-				
-				
-				
